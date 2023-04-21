c4947ac7d569e626b011c776451413750c266ebc
gshshshvq <131294642+gshshshvq@users.noreply.github.com>
2023-04-21 10:37:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+
+    API_ID = 1518984
+
+    API_HASH = "a4ab7c25e6d6c5a269b0522682551202"
+
+    TOKEN = "6124983482:AAEw1jQBx6-LZgdBgrHKm502wf2xud66c7g"  
+
+    START_PIC = "" 
+
+    CHAT = ""    
+
