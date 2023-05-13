d5e9fd955114cbb99d91c0a81a10497499e7d22d
gshshshvq <131294642+gshshshvq@users.noreply.github.com>
2023-05-13 17:25:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config:
 
-    API_ID = 
+    API_ID   = 23843216 
 
-    API_HASH = ""
+    API_HASH = "6f5e604a5082cbe15df96042882ac8ad"
 
-    TOKEN = ""  
+    TOKEN = "6189942182:AAF33b9kqizmq6kjVaAmCXiCL0mz-QlGb1g"  
 
     START_PIC = "" 
 
